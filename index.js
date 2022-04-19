29f9e939d8f48e17ef4302777e8b8211d54010e6
Towew <doublezx321@gmail.com>
2022-04-19 22:10:56 +0700

    ACAO

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ const router = require('./src/routes');
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //Gunakan fungsi route yang sudah dijadikan variabel
 app.use('/api/v1/', router); 
 app.use('/uploads', express.static('uploads'));
